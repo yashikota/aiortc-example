b31842f9077277cc238634c20d0e50428ac50a52
yashikota <52403688+yashikota@users.noreply.github.com>
2026-02-20 11:30:54 +0900

    https

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // server: {
+  //   https: true,
   //   watch: {
   //     usePolling: true,
   //     interval: 100
